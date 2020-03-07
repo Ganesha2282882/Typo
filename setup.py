71e58f1b60a84a76089ae3509a25b523403890b9
Ganesha Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2020-03-07 15:12:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-Ganesha-Sharma", # Ganesha Sharma
+    name="typopoo-Ganesha-Sharma", # Ganesha Sharma
     version="0.0.1",
     author="Ganesha Sharma",
     author_email="sharmaganesha2@gmail.com",
